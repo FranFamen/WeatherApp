f4dce3fc5b0619057ecbb6c4725b288430a5a755
Czesiu <jedrzej.jusza@outlook.com>
2018-11-16 21:00:45 +0100

    Added Dependency Injection bootstrapping, created necessary folders for View Model location

--- WeatherApp/App.xaml.cs
+++ WeatherApp/App.xaml.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using Prism.Ioc;
+using WeatherApp.Views;
 
 namespace WeatherApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
     }
 }
---
+++ WeatherApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+using Prism.Mvvm;
+
+namespace WeatherApp.ViewModels
+{
+	public class MainWindowViewModel : BindableBase
+	{
+		
+	}
+}
